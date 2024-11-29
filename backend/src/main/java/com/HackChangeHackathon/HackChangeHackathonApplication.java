eae018875aa85663e80de443e7e41cc723bf6c9a
Roman Ryabikin <riabikin.roman@gmail.com>
2024-11-29 22:54:32 +0300

    Initial commit

---
+++ backend/src/main/java/com/HackChangeHackathon/HackChangeHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.HackChangeHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackChangeHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackChangeHackathonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/HackChangeHackathon/HackChangeHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HackChangeHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackChangeHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
